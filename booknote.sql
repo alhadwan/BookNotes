13e3b9bf80fd420fb6ef33d9c22fa344d8d7c57e
Ali Hadwan <alihadwan@Alis-MacBook-Pro.local>
2025-04-14 22:56:43 -0700

    frontend/backend/database inital setup

---
+++ booknote.sql
@@ -0,0 +1,11 @@
+CREATE TABLE books (
+  id SERIAL PRIMARY KEY,
+  title TEXT NOT NULL,
+  notes TEXT,
+  rating INTEGER CHECK (rating BETWEEN 1 AND 5),
+  read_date DATE,
+  isbn TEXT
+);
+
+INSERT INTO table_name (title, notes, rating, read_date, isbn)
+VALUES ('rich dad poor dad', 'this is a great finance book', 5, '2025-04-01', '235624562');
