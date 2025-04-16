aa3aaad2474a24308873159e347d55818493f4ce
Ali Hadwan <alihadwan@Alis-MacBook-Pro.local>
2025-04-15 17:41:32 -0700

    index.ejs:include title, notes, rating, read date, index.js:GET/Show list of books, database:quary

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.set("view engine", "ejs");
 
 const db = new pg.Client({
     user: "postgres",
@@ -17,10 +18,19 @@ const db = new pg.Client({
   });
   
   db.connect();
+let books = [];
 
-app.get("/", (req, res) =>{
-    res.send("hello, express");
+app.get("/", async(req, res) =>{
+    try{
+        const result = await db.query("SELECT * FROM books");
+         books = result.rows;
+        console.log(books);
+        res.render("index.ejs", {book: books});
+    } catch(err){
+        console.error("Error fetching books:", err);
+    }
 });
+
 
 app.listen(port, () => {
     console.log(`listening on port ${port}`);
