82b8e062234e154915a5dff5ae96d22af9dcc5a1
Ali Hadwan <alihadwan@Alis-MacBook-Pro.local>
2025-04-17 04:07:06 -0700

    Add new book form, add GET/Show list of books - POST/add Add a new book and Sort dropdown (by rating, date, or title)

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
+import axios from "axios";
 
 const app = express();
 const port = 3000;
@@ -18,11 +19,26 @@ const db = new pg.Client({
   });
   
   db.connect();
-let books = [];
+let books = [
+    // {id: 1,
+    // title: 'rich dad poor dad',
+    // notes: 'this is a great finance book',
+    // rating: 5,
+    // read_date: 2025-04-01T07:00:00.000Z,
+    // isbn: '1612681131'}
+];
 
 app.get("/", async(req, res) =>{
     try{
-        const result = await db.query("SELECT * FROM books");
+        const sort = req.query.sort;
+        let orderby = "title ASC";
+        if(sort === "rating"){
+            orderby = "rating DESC"
+        }else if(sort === "date"){
+            orderby = "read_date DESC"
+        }
+        console.log(orderby);
+        const result = await db.query(`SELECT * FROM books ORDER By ${orderby}`);
          books = result.rows;
         console.log(books);
         res.render("index.ejs", {book: books});
@@ -30,6 +46,26 @@ app.get("/", async(req, res) =>{
         console.error("Error fetching books:", err);
     }
 });
+
+app.get("/add", (req, res) => {
+    res.render("form.ejs");
+  });
+
+  app.post("/submit", async(req, res) => {
+    const booktitle = req.body.booktitle;
+    const booknotes = req.body.booknotes;
+    const rating = req.body.rating;
+    const date = req.body.date;
+    console.log(date);
+    const bookisbn = req.body.bookisbn;
+
+    const result = await db.query("INSERT INTO books (title, notes, rating, read_date, isbn) VALUES ($1, $2, $3, $4, $5)",[booktitle, booknotes, rating, date, bookisbn]);
+    const Data = result.rows;
+    console.log(Data);
+
+    res.redirect("/");
+  });
+
 
 
 app.listen(port, () => {
